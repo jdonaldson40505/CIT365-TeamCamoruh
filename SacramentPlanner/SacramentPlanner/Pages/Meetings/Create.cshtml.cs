53031cab5272aeb1ecd834c866f51a46d3ac275e
jdonaldson40505 <don16009@byui.edu>
2022-12-07 23:19:26 -0700

    added to our landing page

--- SacramentPlanner/SacramentPlanner/Pages/Meetings/Create.cshtml.cs
+++ SacramentPlanner/SacramentPlanner/Pages/Meetings/Create.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -61,6 +62,7 @@ namespace SacramentPlanner.Pages.Meetings
             _context.Meeting.Add(Meeting);
             //_context.Speaker.Add(Speaker);
             await _context.SaveChangesAsync();
+            
             return RedirectToPage("./Index");
         }
          
